3441c15b6aef1482d6f976f1dba209b587dba4ee
dchan3 <dchan3@hawaii.edu>
2015-09-20 15:47:26 -1000

    A07

--- A07/myScript.js
+++ A07/myScript.js
@@ -13,7 +13,7 @@ function addClass(elem, className) {
     var a = tagContents.indexOf("class")
     if (a >= 0) {
         var classesString = tagContents.substring(tagContents.indexOf("\"", tagContents.indexOf("=", tagContents.indexOf("class"))) + 1, tagContents.indexOf("\"", tagContents.indexOf("\"", tagContents.indexOf("=", tagContents.indexOf("class"))) + 1)).replace('\"', '');
-        if (classesString.search(className) < 0) {
+        if (getClasses(elem).indexOf("\b" + className + "\b") >= 0) {
             var newClasses = classesString + " " + className;
             tagContents = tagContents.replace(classesString, newClasses);
         }
@@ -30,28 +30,83 @@ function removeClass(elem, className) {
     var a = tagContents.indexOf("class")
     if (a >= 0) {
         var classesString = tagContents.substring(tagContents.indexOf("\"", tagContents.indexOf("=", tagContents.indexOf("class"))) + 1, tagContents.indexOf("\"", tagContents.indexOf("\"", tagContents.indexOf("=", tagContents.indexOf("class"))) + 1)).replace('\"', '');
-        if (classesString.search("\b" + className + "\b") < 0) {
+        if (getClasses(elem).indexOf("\b" + className + "\b") < 0) {
             var newClasses = classesString.replace(className, '');
             tagContents = tagContents.replace(classesString, newClasses);
         }
-    }
-    else {
-        tagContents += " class=\"" + className + "\""
     }
     return elem.replace(oldContents, tagContents)
 }
 
 function validateForm() {
-    var errors = {};
-    var retval;
+    var errors;
+    errors = [];
     var i = 0;
-    if (document.getElementById('username').value == "") {
-        document.getElementById('username').outerHTML =  addClass(document.getElementById('username').outerHTML, "error_field");
-        errors[i] =  "Enter a username.";
+    var val = "";
+    var val1 = "";
+    var val2 = "";
+
+    val = document.getElementById('username').value;
+    if (!val) {
+        document.getElementById('username').outerHTML = addClass(document.getElementById('username').outerHTML, "error_field");
+        errors[i] = "Enter a username.";
         i++;
     }
     else {
         document.getElementById('username').outerHTML = removeClass(document.getElementById('username').outerHTML, "error_field");
     }
-    if (i > 0) return false;
+    document.getElementById('username').setAttribute("value", val);
+
+    val = document.getElementById('email').value;
+    if (!val) {
+        document.getElementById('email').outerHTML =  addClass(document.getElementById('email').outerHTML, "error_field");
+        errors[i] = "Enter a valid email address.";
+        i++;
+    }
+    else {
+        document.getElementById('email').outerHTML = removeClass(document.getElementById('email').outerHTML, "error_field");
+    }
+    document.getElementById('email').setAttribute("value", val);
+
+    val1 = document.getElementById('password').value;
+    val2 = document.getElementById('confirm').value;
+    if (val1 !== val2) {
+        if (!val1) {
+            document.getElementById('password').outerHTML = addClass(document.getElementById('password').outerHTML, "error_field");
+            errors[i] = "Enter a password before confirming it."
+        }
+        else if (!val2) {
+            document.getElementById('confirm').outerHTML = addClass(document.getElementById('confirm').outerHTML, "error_field");
+            errors[i] = "Confirm your password.";
+        }
+        else {
+            document.getElementById('password').outerHTML = addClass(document.getElementById('password').outerHTML, "error_field");
+            document.getElementById('confirm').outerHTML = addClass(document.getElementById('confirm').outerHTML, "error_field");
+            errors[i] = "Password mismatch.";
+        }
+        i++;
+    }
+    else {
+        if (!val1 && !val2) {
+            document.getElementById('password').outerHTML = addClass(document.getElementById('password').outerHTML, "error_field");
+            document.getElementById('confirm').outerHTML = addClass(document.getElementById('confirm').outerHTML, "error_field");
+            errors[i] = "Password fields empty.";
+            i++;
+        }
+        else {
+            document.getElementById('password').outerHTML = removeClass(document.getElementById('password').outerHTML, "error_field");
+            document.getElementById('confirm').outerHTML = removeClass(document.getElementById('confirm').outerHTML, "error_field");
+        }
+    }
+    document.getElementById('password').setAttribute("value", val1);
+    document.getElementById('confirm').setAttribute("value", val2);
+    document.getElementById("error").innerHTML = "";
+    if (i > 0) {
+        document.getElementById("error").innerHTML += "Error(s): \<ul\>";
+        for (var e = 0; e < errors.length; e++) {
+            document.getElementById("error").innerHTML += "\<li\>" + errors[e] + "\</sli\>";
+        }
+        document.getElementById("error").innerHTML += "\</ul\>";
+    }
+    return (i == 0);
 }
